cfffd4a7586db1ce6228faf415b942828346da01
Beren Erchamion <beren@houseofbeor.net>
2021-11-27 16:59:15 -0500

    Adding dotenv and providers for posts and post.

---
+++ lib/providers/post_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Post with ChangeNotifier {
+  final int id;
+  final String title;
+  final DateTime created;
+  final DateTime updated;
+  final String imageUrl;
+  bool? isFavorite;
+
+  Post({
+    required this.id,
+    required this.title,
+    required this.created,
+    required this.updated,
+    required this.imageUrl,
+    isFavorite = false,
+  });
+}
---
+++ lib/providers/posts_provider.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class Posts with ChangeNotifier {
+
+}
